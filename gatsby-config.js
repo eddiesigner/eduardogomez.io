3c3ac4d305ca2e02b2f8811c643c52a8394a983d
Eduardo Gomez <this.eduardo@gmail.com>
2020-12-29 19:36:57 +0100

    Remove manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,30 +107,6 @@ module.exports = {
      *  Utility Plugins
      */
     {
-      resolve: `gatsby-plugin-ghost-manifest`,
-      options: {
-        short_name: config.shortTitle,
-        start_url: `/`,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: `minimal-ui`,
-        icon: `static/${config.siteIcon}`,
-        legacy: true,
-        query: `
-          {
-            allGhostSettings {
-              edges {
-                node {
-                  title
-                  description
-                }
-              }
-            }
-          }
-        `,
-      },
-    },
-    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
