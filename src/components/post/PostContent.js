9962cb443c0e76780be84a2d1a12e6315d70d312
Eduardo Gomez <this.eduardo@gmail.com>
2021-05-29 19:40:41 +0200

    Try to render the post content after build

--- src/components/post/PostContent.js
+++ src/components/post/PostContent.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import InnerHTML from 'dangerously-set-html-content'
 
 const PostContent = ({ html }) => (
   <section className="post-full-content">
-    <InnerHTML className="content-body load-external-scripts" html={html} />
+    <div
+      className="content-body load-external-scripts"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
   </section>
 )
 
